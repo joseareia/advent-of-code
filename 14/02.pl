fcba0333853953bbb5732c00ea22c9a688cfdd07
joseareia <jose.apareia@gmail.com>
2024-12-14 20:46:01 +0000

    add: solution for day/puzzle [14/02]

---
+++ 14/02.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Image::Magick;
+
+open(my $in, '<', 'input.txt') or die $!;
+my @robots = map {/p=(-?\d+),(-?\d+)\s+v=(-?\d+),(-?\d+)/ ? {x=>$1, y=>$2, vx=>$3, vy=>$4} : ()} <$in>;
+my ($width, $height, $time) = (101, 103, 0);
+
+while (1) {
+    my @grid = map { [(".") x $width] } 1 .. $height;
+
+    foreach my $robot (@robots) {
+        my $new_x = ($robot->{x} + $robot->{vx} * $time) % $width;
+        my $new_y = ($robot->{y} + $robot->{vy} * $time) % $height;
+        $new_x += $width if $new_x < 0;
+        $new_y += $height if $new_y < 0;
+        $grid[$new_y][$new_x] = '#';
+    }
+
+    my $found = 0;
+    foreach my $row (@grid) {
+        my $line = join("", @$row);
+        if ($line =~ /#{10,}/) {
+            $found = 1;
+            last;
+        }
+    }
+
+    if ($found) {
+        foreach my $row (@grid) {
+            print join("", @$row), "\n";
+        }
+        print "Part 02 : $time\n";
+        last;
+    }
+    print $time, "\n";
+    $time++;
+}
